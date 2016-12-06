e57d3650c645e6efee0dc4debf643b0890b070b4
kurogosane1 <magnite@hotmail.com>
2016-12-05 22:49:09 -0600

    added the JS and html files

---
+++ response.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+
+// Process of how this will be perfomred:
+// 1. First the user shall enter the data of the train in the input text areas found and once the user clicks submit
+// 2. The system will verify if the user entered all the data and that all the input area's are submitted.
+// 3. if the user has entered all the data in the input area the n the data is then forwared to the firebase system where the data would then be stored
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
